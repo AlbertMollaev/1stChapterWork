621820180c9c4a1f8c11e6457bb43cf8cf6bf485
AlbertM <mollaev702@gmail.com>
2023-09-03 18:16:40 +0300

    Написали функцию SecondArray для создания нового массива из заданного пользователем и функцию PrintArray для вывода полученного массива

--- Final work/Task1/Program.cs
+++ Final work/Task1/Program.cs
@@ -1,5 +1,27 @@
 ﻿
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+    
+}
 
+
+void SecondArray(string[] massiv, string[] new_massiv)
+{
+    int count = 0;
+    for (int i = 0; i < massiv.Length; i++)
+    {
+    if(massiv[i].Length <= 3)
+        {
+        new_massiv[count] = massiv[i];
+        count++;
+        }
+    }
+}  
 
 Console.WriteLine("Введите размер массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
@@ -10,3 +32,8 @@ for (int i = 0; i < massiv.Length; i++)
 }
 
 string[] new_massiv = new string[massiv.Length];
+
+SecondArray(massiv, new_massiv);
+Console.WriteLine($"В массиве [{string.Join(",", massiv)}] строки с колличеством символов 3 либо меньше : ");
+PrintArray(new_massiv);
+
