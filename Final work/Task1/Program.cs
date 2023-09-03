5c95a296f9926fe58f279434b8ce2e41b830b089
AlbertM <mollaev702@gmail.com>
2023-09-03 18:13:56 +0300

    Начали написание когда: Создали два массива (massiv, new_massiv) и написали код запроса данных для заполнения массива с клавиатуры

--- Final work/Task1/Program.cs
+++ Final work/Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] massiv = new string[n]; //{"Hello", "asdsd", "world", "add22", "Hello"};
+for (int i = 0; i < massiv.Length; i++)
+{
+    massiv[i] = Console.ReadLine()!;
+}
+
+string[] new_massiv = new string[massiv.Length];
